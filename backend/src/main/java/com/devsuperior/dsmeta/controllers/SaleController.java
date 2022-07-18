78653140011d1b67500865924fece6fb555b9b2f
André Matos <andreluisv8@gmail.com>
2022-07-17 23:51:10 -0300

    COMMIT: Date select

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -3,11 +3,14 @@ package com.devsuperior.dsmeta.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dsmeta.entities.Sales;
+import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.services.SaleService;
 
 @RestController
@@ -18,8 +21,11 @@ public class SaleController {
 	private SaleService service;
 	
 	@GetMapping
-	public List<Sales> findSales(){
-		return service.findSales();
+	public Page<Sale> findSales(
+			@RequestParam(value = "minDate", defaultValue = "") String mindate, 
+			@RequestParam(value = "maxDate", defaultValue = "") String maxdate,                                                    String maxDate,
+			Pageable pageable){
+		return service.findSales(mindate, maxdate, pageable);
 		
 	}
 
--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,13 +1,18 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-//import org.springframework.data.jpa.repository.cdi.JpaRepositoryExtension;
+import org.springframework.data.jpa.repository.cdi.JpaRepositoryExtension;
 
-import com.devsuperior.dsmeta.entities.Sales;
+import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
 
 @Service
@@ -16,9 +21,14 @@ public class SaleService  {
 	@Autowired
 	private SaleRepository repository;
 
-	public List<Sales> findSales(){
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable){
 		
-		return repository.findAll();
+		LocalDate today  = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("")? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("")? today : LocalDate.parse(maxDate);
+		
+		return repository.findSales(min, max ,pageable);
 		
 	}
 	
